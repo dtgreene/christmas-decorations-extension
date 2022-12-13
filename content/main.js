256c5d123a0dbe2e86e6d2112a50138ad7be15cb
Dylan Greene <dylan.greene@shipt.com>
2022-12-12 20:48:50 -0600

    Add project

---
+++ content/main.js
@@ -0,0 +1,137 @@
+// create root
+const root = document.createElement('div');
+root.className = 'xmas-lights';
+// append to the dom
+document.body.appendChild(root);
+
+const defaultColors = prepareColors([
+  '#188399',
+  '#D44359',
+  '#E8A36A',
+  '#4C38A8',
+  '#9C3161',
+]);
+const defaultSides = { top: true, right: false, bottom: false, left: false };
+
+const settings = {
+  colors: defaultColors,
+  sides: defaultSides,
+};
+
+const spacing = 64;
+const shadow = 8;
+
+// perform setup
+init();
+
+async function init() {
+  await getSettings();
+
+  const { colors } = settings;
+
+  // set the color CSS variables
+  colors.forEach(([color1, color2], index) => {
+    const prefix = `--xmas-color-${index + 1}`;
+    document.body.style.setProperty(prefix, color1);
+    document.body.style.setProperty(`${prefix}-fade`, color2);
+  });
+
+  // set the spacing CSS variable
+  document.body.style.setProperty('--xmas-spacing', `${spacing}px`);
+  document.body.style.setProperty('--xmas-shadow', `${shadow}px`);
+
+  // add resize listener
+  window.addEventListener('resize', createLights, false);
+
+  // initially create the lights
+  createLights();
+}
+
+function createLights() {
+  const cols = Math.floor(window.innerWidth / spacing);
+  const rows = Math.floor(window.innerHeight / spacing) - 1;
+
+  const { sides } = settings;
+
+  // reset the root HTML
+  root.innerHTML = '';
+
+  if (sides.top) {
+    root.innerHTML = root.innerHTML.concat(createLightGroup(cols));
+  }
+  if (sides.right) {
+    root.innerHTML = root.innerHTML.concat(createLightGroup(rows, 'col'));
+  }
+  if (sides.bottom) {
+    root.innerHTML = root.innerHTML.concat(
+      createLightGroup(rows, 'col', 'end')
+    );
+  }
+  if (sides.left) {
+    root.innerHTML = root.innerHTML.concat(
+      createLightGroup(cols, 'row', 'end')
+    );
+  }
+}
+
+function createLightGroup(count, orientation = 'row', position = 'start') {
+  const lights = [];
+
+  for (let i = 0; i < count; i++) {
+    lights.push(
+      [
+        '<div class="xmas-container">',
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" class="xmas-wire">',
+        '<path fill="none" stroke="#294b29" strokeWidth="2" d="M0,32 Q32,64 64,32" />',
+        '</svg>',
+        '<div class="xmas-socket"></div>',
+        '<div class="xmas-bulb"></div>',
+        '</div>',
+      ].join('')
+    );
+  }
+
+  return [
+    `<div class="xmas-light-group xmas-${orientation} xmas-${position}">`,
+    lights.join(''),
+    '</div>',
+  ].join('');
+}
+
+async function getSettings() {
+  const { colors, sides } = await browser.storage.local.get([
+    'colors',
+    'sides',
+  ]);
+
+  if (colors) {
+    settings.colors = prepareColors(colors);
+  }
+  if (sides) {
+    settings.sides = sides;
+  }
+}
+
+function prepareColors(colors) {
+  return colors.map((color1) => [color1, shadeHexColor(color1, -0.2)]);
+}
+
+function shadeHexColor(color, percent) {
+  const f = parseInt(color.slice(1), 16),
+    t = percent < 0 ? 0 : 255,
+    p = percent < 0 ? percent * -1 : percent,
+    R = f >> 16,
+    G = (f >> 8) & 0x00ff,
+    B = f & 0x0000ff;
+  return (
+    '#' +
+    (
+      0x1000000 +
+      (Math.round((t - R) * p) + R) * 0x10000 +
+      (Math.round((t - G) * p) + G) * 0x100 +
+      (Math.round((t - B) * p) + B)
+    )
+      .toString(16)
+      .slice(1)
+  );
+}
---
+++ popup/main.js
@@ -0,0 +1,88 @@
+const defaultColors = ['#188399', '#D44359', '#E8A36A', '#4C38A8', '#9C3161'];
+const defaultSides = { top: true, right: false, bottom: false, left: false };
+
+let settings = {
+  colors: defaultColors,
+  sides: defaultSides,
+};
+
+async function init() {
+  const { colors, sides } = await browser.storage.local.get([
+    'colors',
+    'sides',
+  ]);
+
+  // set default colors
+  if (!colors) {
+    settings.colors = defaultColors;
+    await browser.storage.local.set({ colors: defaultColors });
+  } else {
+    // otherwise, use the value from storage
+    settings.colors = colors;
+  }
+  // set default sides
+  if (!sides) {
+    settings.sides = defaultSides;
+    await browser.storage.local.set({ sides: defaultSides });
+  } else {
+    // otherwise, use the value from storage
+    settings.sides = sides;
+  }
+
+  // init side checkboxes
+  initSideCheckbox(0, settings.sides.top);
+  initSideCheckbox(1, settings.sides.right);
+  initSideCheckbox(2, settings.sides.bottom);
+  initSideCheckbox(3, settings.sides.left);
+
+  // init color inputs
+  initColorInput(0, settings.colors[0]);
+  initColorInput(1, settings.colors[1]);
+  initColorInput(2, settings.colors[2]);
+  initColorInput(3, settings.colors[3]);
+  initColorInput(4, settings.colors[4]);
+}
+
+function initSideCheckbox(index, defaultValue = false) {
+  const element = document.getElementById(`side-${index}-check`);
+  element.addEventListener('change', handleSideChange, false);
+  element.checked = defaultValue;
+}
+
+function initColorInput(index, defaultValue = '') {
+  const element = document.getElementById(`color-${index}-input`);
+  element.addEventListener('change', handleColorChange, false);
+  element.value = defaultValue;
+  const previewElement = document.getElementById(`color-${index}-preview`);
+  previewElement.style.background = defaultValue;
+}
+
+async function handleSideChange(event) {
+  const index = Number(event.target.id.match(/\d+/)[0]);
+  const keys = ['top', 'right', 'bottom', 'left'];
+
+  const key = keys[index];
+
+  if (key) {
+    // set settings
+    settings.sides[key] = event.target.checked;
+    // save settings
+    await browser.storage.local.set({ sides: settings.sides });
+  }
+}
+
+async function handleColorChange(event) {
+  const index = Number(event.target.id.match(/\d+/)[0]);
+
+  if (index >= 0 && index <= 4) {
+    // set preview
+    const previewElement = document.getElementById(`color-${index}-preview`);
+    previewElement.style.background = event.target.value;
+    // set settings
+    settings.colors[index] = event.target.value;
+    // save settings
+    await browser.storage.local.set({ colors: settings.colors });
+  }
+}
+
+window.onload = init;
